7a1f8e0584deccd1d92aeea1aaa5139902794e7a
snehalbabar <h728e344@wichita.edu>
2023-06-18 12:08:50 -0500

    Example of urls redirection

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UrlRedirection.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        [Route("Bookstore")]
+        public IActionResult Index()
+        {
+            // return new RedirectToActionResult("Index","Store", new { }); // return 302 result temporarily moved
+
+            return new RedirectToActionResult("Index", "Store", new { }, permanent:true);  // return 301 result permentaly moved
+
+        }
+    }
+}
+
---
+++ Controllers/StoreController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UrlRedirection.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: /<controller>/
+        [Route("store/books")]
+        public IActionResult Index()
+        {
+            return Content("<h1>Book Store</h1>", "text/html");
+        }
+    }
+}
+
