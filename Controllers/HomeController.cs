d84f613ed4a2396b23a348786f0abdd85585e2d5
snehalbabar <h728e344@wichita.edu>
2023-06-22 12:31:43 -0500

    Adding qrueyfrom data to the controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UrlRedirection.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,13 +12,16 @@ namespace UrlRedirection.Controllers
     public class HomeController : Controller
     {
         // GET: /<controller>/
-        [Route("Bookstore")]
-        public IActionResult Index()
+        [Route("Bookstore/{bookid?}/{isloggedIn?}")]
+
+        public IActionResult Index([FromRoute]int? bookid, [FromRoute ]bool? isloggedIn,
+             Book book)
         {
             // return new RedirectToActionResult("Index","Store", new { }); // return 302 result temporarily moved
 
-            return new RedirectToActionResult("Index", "Store", new { }, permanent:true);  // return 301 result permentaly moved
+            // return new RedirectToActionResult("Index", "Store", new { }, permanent:true);  // return 301 result permentaly moved
 
+            return Content("Route parameter Example"); 
         }
     }
 }
---
+++ Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace UrlRedirection.Models
+{
+	public class Book
+	{
+		public int? BookId { get; set; }
+
+		public string? Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"Book Object - book ID : {BookId}, Author :{Author}";
+        }
+
+
+    }
+}
+
